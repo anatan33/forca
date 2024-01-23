fb7178545f462ec5a68f3a2b2fb88a706015861d
Anatan Maia <anatanmaia3302@gmail.com>
2024-01-23 00:16:31 -0300

    Código refatorado

--- main.kt
+++ main.kt
@@ -3,10 +3,8 @@ import kotlin.random.Random
 fun main() {
     val words = listOf("Code", "Coffe", "Bug")
 
-    /*
-    * feat para obter uma palavra aleatória
-    */
+    //Retorna uma palavra aleatóio dentro de uma lista de palavras
+    fun getRandomWord(value: Int): String = words.get(Random.nextInt(0, value))
 
-    fun getWord(value: Int):Int = Random.nextInt(0, value)
-    println(getWord(words.size))
+    println(getRandomWord(words.size))
 }
